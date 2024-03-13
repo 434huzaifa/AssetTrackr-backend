f9d2f815e90862ae5ae6a6f28e047c7098d09226
Md. Huzaifa <saadhuzaifa2497@gmail.com>
2024-03-14 01:38:50 +0600

    Company model added

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.db.models import CharField,EmailField,DecimalField,TextField,UUIDField
+import uuid
 # Create your models here.
+class Company(models.Model):
+    id=UUIDField(primary_key=True,default=uuid.uuid4(),editable=False)
+    title=CharField(max_length=150,null=True,blank=True)
+    email=EmailField(null=True,blank=True,unique=True)
+    password=CharField(max_length=10,null=True,blank=True)
